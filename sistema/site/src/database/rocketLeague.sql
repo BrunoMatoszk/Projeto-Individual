86e3d30e046db5348b19e2c0cc6725c4881cfede
BrunoMatoszk <bruno.matos@sptech.school>
2023-11-30 22:27:41 -0300

    projeto finalizado

--- sistema/site/src/database/rocketLeague.sql
+++ sistema/site/src/database/rocketLeague.sql
@@ -58,12 +58,47 @@ comandos para criar usuário em banco de dados azure, sqlserver,
 com permissão de insert + update + delete + select
 */
 
-CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
-WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
-DEFAULT_SCHEMA = dbo;
-
-EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
-
-EXEC sys.sp_addrolemember @rolename = N'db_datareader',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+  
+        create view votoDoUsuario as
+         select idUsuario, usuario.nome, usuario.fkJogador, jogador.nome as NomeJogador from usuario join jogador
+            on fkJogador = idJogador
+                where idUsuario = 1; -- mostra o jogador que o usuário votou
+                
+                
+                
+    create view totalAvaliacoesJogador as
+    select jogador.nome as NomeJogador, COUNT(fkPlayer) as TotalJogador from avaliacao 
+        join jogador ON fkPlayer = idJogador group by jogador.nome 
+        order by TotalJogador desc; -- conta o total de avaliações por jogador
+        
+        
+        create view totalVotosJogador as
+        select jogador.nome as NomeJogador, count(fkJogador) as TotalVotos from usuario join jogador
+        on fkJogador = idJogador
+            group by jogador.nome order by TotalVotos desc; -- conta o total de votos por jogador
+            
+            
+        create view avaliacoesDeUmUsuario as
+        select a.idAvaliacao, a.descricao, u.idUsuario, u.nome, u.sobrenome, u.email, u.senha, a.fkPlayer, 
+    j.nome as NomeJogador 
+    from avaliacao a join jogador j
+    on fkPlayer = idJogador join usuario u on a.fkUser = u.idUsuario where u.idUsuario = 1; -- mostra as avaliações de um usuário com nome do jogador
+    
+    
+    
+    create view avaliacoesComNomeJogador as 
+    select a.idAvaliacao, a.descricao, u.idUsuario, u.nome, u.sobrenome, u.email, u.senha, a.fkPlayer, 
+    j.nome as NomeJogador 
+    from avaliacao a join jogador j
+    on fkPlayer = idJogador join usuario u on a.fkUser = u.idUsuario; -- mostra as avaliações com nome do jogador
+    
+    
+		
+	create view rankingQuiz as select 
+    u.nome as 'Usuário',
+    p.acertos 'Acertos',
+    p.pontuacao as 'Pontuação',
+    j.nome as 'Jogador'
+    from pontuacaoquiz as p join usuario as u
+	on fkUsuario = idUsuario
+    join jogador as j on idJogador = fkJogador order by pontuacao desc; -- ranking do quiz
